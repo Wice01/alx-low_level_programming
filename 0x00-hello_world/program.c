23d64fbe61756168be78af2e21dc0454b9b82cc9
Imolorhe Ohiomoba <imolorheohiomoba@gmail.com>
2022-09-08 10:09:07 -0700

    C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line

---
+++ 0x00-hello_world/program.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always zero (Success)
+ */
+int main()
+{
+	char str[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(str);
+
+	return 0;
+}
