2388255b04b3c52a41ccdef41f417675f49c5fde
Imolorhe Ohiomoba <imolorheohiomoba@gmail.com>
2022-09-09 18:07:10 -0700

    program that prints all possible different combinations of three digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Entry poiint 
+ * main - Entry poiint
  *
  * Return: must be equal to 0
  */
@@ -11,15 +11,16 @@ int main(void)
 	for (digit1 = 0; digit1 < 9; digit1++)
 	{
 		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
-		{	
+		{
 			putchar((digit1 % 10) + '0');
 		        putchar((digit2 % 10) + '0');
 
 		        if (digit1 == 8 && digit2 == 9)
 				continue;
-		        putchar(',');
+
+			putchar(',');
 		        putchar(' ');
-                 }
+                }
 	}
 	putchar('\n');
 	return (0);
