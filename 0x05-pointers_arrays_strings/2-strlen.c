e243d397868ab23ee6aa705a7ebc7a60d555cfe1
Imolorhe Ohiomoba <imolorheohiomoba@gmail.com>
2022-09-19 12:52:04 -0700

    function that returns the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - prints lenght of a string
+ * @s: character pointer
+ */
+
+int _strlen(char *s)
+{
+	int count;
+
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+	return (0);
+}
