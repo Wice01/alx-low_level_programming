4b58aff15b912d49b9178322f8b47f08494a1efc
Imolorhe Ohiomoba <imolorheohiomoba@gmail.com>
2022-09-21 13:49:11 -0700

    function that concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,16 +12,16 @@ char *_strcat(char *dest, char *src)
 	int a = 0;
 	int b = 0;
 
-	while (src[a] != '\0')
+	while (dest[a] != '\0')
 	{
 		a++;
 	}
-	while (dest[b] != '\0')
+	while (src[b] != '\0')
 	{
 		b++;
 		a++;
-		src[a] = dest[b];
+		dest[a] = src[b];
 	}
-	src = '\0';
-	return (0);
+	dest = '\0';
+	return (dest);
 }
