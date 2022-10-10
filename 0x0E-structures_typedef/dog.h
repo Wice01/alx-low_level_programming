0aa34efa70a8189705d5efc4448a065df1f72ab4
Imolorhe Ohiomoba <imolorheohiomoba@gmail.com>
2022-10-10 22:50:49 +0100

    new type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,12 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef FILE_DOG
+#define FILE_DOG
+
+/**
+ * struct dog - structure dog
+ * @name: atribute pointer to char name's dog
+ * @age: atribute float age's dog
+ * @owner: atribute pointer to char owner's dog
+ */
 
 struct dog
 {
@@ -8,5 +15,15 @@ struct dog
     char * owner;
     /*struct for dog*/
 };
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+void print_dog(struct dog *d);
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
 
 #endif
