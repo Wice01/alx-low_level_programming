384db65656e5fd7b9b5c565fadff36f3d4d31738
Imolorhe Ohiomoba <imolorheohiomoba@gmail.com>
2022-10-12 21:18:34 +0100

    updates

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 
 /**
- * print_name - Prints a name.
+ * print_name - Prints the name 
  * @name: The name to be printed.
  * @f: A pointer to a function that prints a name.
  */
