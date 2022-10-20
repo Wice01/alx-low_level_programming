c65961db1ecb2fc0b4f68063769bda8e96535022
Imolorhe Ohiomoba <imolorheohiomoba@gmail.com>
2022-10-20 15:42:36 -0700

    updates

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+
 /**
  * list_len - returns then number of elements in a list.
  * @h: singly linked list.
@@ -7,13 +8,13 @@
 
 size_t list_len(const list_t *h)
 {
-    size_t nelem;
+	size_t nelem;
 
-    nelem = 0;
-    while (h != NULL)
-    {
-        h = h->next;
-        nelem++;
-    }
-    return (nelem);
+	nelem = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		nelem++;
+	}
+	return (nelem);
 }
