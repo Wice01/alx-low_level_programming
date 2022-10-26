7710fc2314f7db034344e40cd70f6777f813b142
Imolorhe Ohiomoba <imolorheohiomoba@gmail.com>
2022-10-26 15:21:02 -0700

    files

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,14 +9,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-    size_t num = 0;
+	size_t num = 0;
 
-    while (h)
-    {
-        printf("%d\n", h->n);
+	while (h)
+	{
+	printf("%d\n", h->n);
         num++;
-        h = h->next;
-    }
-
-    return (num);
+	h = h->next;
+	}
+	
+	return (num);
 }
